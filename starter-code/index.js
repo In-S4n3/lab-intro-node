162f5bbca04ee83195b0c63cd26a554beb4907e4
In-S4n3 <tiagodcpp@gmail.com>
2019-11-17 17:24:00 +0000

    lab done!

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,51 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+      this.length = 0;
+    }
+    add(x) {
+      this.items.push(x);
+      this.length++;
+      this.items.sort((a, b) => {
+        return a - b;
+      });
+    }
+
+    get(pos) {
+     return this.items[pos];
+    }
+    
+    max() {
+      if (this.items.length > 0 ) {
+       return this.items.reduce(function(a ,b) {
+          return Math.max(a, b)
+        })
+      } else {
+        throw new Error("EmptySortedList");
+      }
+      
+    }
+    min() {
+      if (this.items.length > 0 ) {
+        return this.items.reduce(function(a ,b) {
+          return Math.min(a, b)
+        })
+      } else {
+        throw new Error("EmptySortedList");
+      }
+    }
+
+    sum() {
+     return this.items.reduce((a, b) => a + b, 0) 
+    }
+
+    average() {
+      if (this.items.length > 0 ) {
+        return this.sum()/this.length;
+      } else {
+        throw new Error("EmptySortedList");
+      }
+    }
   };
   
   module.exports = SortedList;
